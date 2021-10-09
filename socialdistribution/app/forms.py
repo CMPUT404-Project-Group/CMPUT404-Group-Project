27c0ce8900de56d6817f65062a37b3533eb29ce6
Dalton Ronan <dronan@ualberta.ca>
2021-10-09 09:39:09 -0600

    update registration form

--- socialdistribution/app/forms.py
+++ socialdistribution/app/forms.py
@@ -10,12 +10,12 @@ class RegisterForm(UserCreationForm):
         self.label_suffix = ''
         # https://stackoverflow.com/a/46283680 - CC BY-SA 3.0
         for field in self.fields.values():
-            field.widget.attrs['class'] = 'input'
+            field.widget.attrs['class'] = 'form-control'
 
-    github_username = forms.CharField(max_length=30, required=False)
+    github = forms.CharField(max_length=30, required=False)
     email = forms.EmailField(max_length=255, required=True)
     username = forms.CharField(max_length=30, required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'github_username', 'password1', 'password2')
+        fields = ('username', 'email', 'github', 'password1', 'password2')
