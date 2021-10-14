49392ea27fe10238ad96fa57a774b915374d5351
Chase-Warwick <warwick@ualberta.ca>
2021-10-14 00:07:11 -0600

    Added post builder to abstract away building a post model object. My rationale was mostly fuelled by it being one of the more complex objects out of most model objects (at least in terms of size) it seemed rather messy to handle all of it inside post manager and so I built a seperate object to handle construction which post manager uses to construct a post

--- socialdistribution/api/models.py
+++ socialdistribution/api/models.py
@@ -1,8 +1,10 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.base_user import BaseUserManager
 import os
 from uuid import uuid4
+from django.db.models.manager import BaseManager
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -91,13 +93,104 @@ class User(AbstractBaseUser):
         # Simplest possible answer: All admins are staff
         return self.is_admin
 
+class PostBuilder():
+
+    def __init__(self):
+        self.uuid = uuid4()
+
+        post_url = f"{HOST_API_URL}posts/{self.uuid}"
+
+        self.type = 'post'
+        self.source = post_url
+        self.origin = post_url
+        self.count = 0
+        self.comment_page = f"{post_url}/comments"
+
+        self.id = None
+        self.title = None
+        self.description = None
+        self.content_type = None
+        self.text_content = None
+        self.image_content = None
+        self.author = None
+        self.categories = None
+        self.size = None
+        self.visibility = None
+        self.unlisted = None
+    
+    def set_post_content(self, title, categories, text_content=None, image_content=None):
+        if not image_content and not text_content:
+            raise ValidationError
+        
+        self.title = title
+        self.categories = categories
+        self.text_content = text_content
+        self.image_content = image_content
+    
+    #TODO: Description is not very descriptive
+    def set_post_metadata(self, author, visibility, unlisted):
+        assert self.title, "set_post_content must be called before set_post_metadata"
+
+        self.author = author
+        self.id = f"{HOST_API_URL}author/{self.author.id}/posts/{self.uuid}"
+        self.visibility = visibility
+        self.unlisted = unlisted
+
+        self.__set_description__()
+        self.__set_content_type__()
+        self.__set_size__()
+    
+    def get_post(self):
+        return Post.objects.create(
+            type=self.type,
+            title=self.title,
+            id=self.id,
+            source=self.source,
+            origin=self.origin,
+            description=self.description,
+            content_type=self.content_type,
+            text_content=self.text_content,
+            image_content=self.image_content,
+            author=self.author,
+            categories=self.categories,
+            count=self.count,
+            size=self.size(),
+            comment_page=self.comment_page,
+            visibility=self.visibility,
+            unlisted=self.unlisted
+        )
+    
+    def __set_description__(self):
+        self.description = f"{self.title}"
+        if self.text_content:
+            self.description += f": {self.text_content[0:50]}..."
+    
+    #TODO: Content type just defaults to plain text at the moment
+    def __set_content_type__(self):
+        self.content_type = Post.ContentType.PLAIN
+    
+    #TODO: Figure out size of post dynamically (possibly iterate through attributes adding size)
+    def __set_size__(self):
+        self.size = 0
+
+
+     
+
+class PostManager(BaseManager):
+
+    def create_post(self, author, categories, image_content, text_content, title, visibility, unlisted):
+        post_builder = PostBuilder()
+        post_builder.set_post_content(title, categories, text_content, image_content)
+        post_builder.set_post_metadata(author, visibility, unlisted)
+        
+        return post_builder.get_post()
+
 #TODO: Specify uploadto field for image_content to post_imgs within project root
 #TODO: Upon adding comment model add comment as foreign key
-#TODO: Figure out page size
 class Post(models.Model):
 
-    class Visibility(models.IntegerChoices):
-        PUBLIC = 0
+    class Visibility(models.TextChoices):
+        PUBLIC = "PUBLIC"
     
     class ContentType(models.TextChoices):
         MARKDOWN = "text/markdown"
@@ -132,52 +225,8 @@ class Post(models.Model):
     visibility = models.IntegerField(choices=Visibility.choices, unique=False, blank=False, null=False, default=Visibility.PUBLIC)
     unlisted = models.BooleanField(unique=False, blank=False, null=False, default=False)
 
-    def clean(self):
-        self.clean_id()
-        self.clean_urls()
-        self.clean_content_type()
-        self.clean_description()
-        self.clean_size()
-    
-    def clean_id(self):
-        self.id = f"{HOST_API_URL}author/{self.author.id}/posts/{uuid4()}"
-    
-    def clean_urls(self):
-        post_url = f"{HOST_API_URL}posts/{self.id}"
-        url_fields = [self.source, self.origin, self.comment_page]
-        if not self.source:
-            self.source = post_url
-        if not self.origin:
-            self.origin = post_url
-        if not self.comment_page:
-            self.comment_page = f"{post_url}/comments"
-    
-    def clean_content_type(self):
-        if self.text_content:
-            self.content_type = self.ContentType.PLAIN
-        elif self.image_content:
-            self.clean_content_type_image()
+    objects = PostManager
 
-    def clean_content_type_image(self):
-        file_name = self.image_content.filename
-        file_extension = file_name[:file_name.rfind('.')]
-
-        if file_extension == 'png':
-            self.content_type = self.ContentType.PNG
-        else:
-            self.content_type = self.ContentType.JPG
-    
-    def clean_description(self):
-        if self.description == "" or not self.description:
-            self.description = str(self)
-
-    def clean_size(self):
-        if self.size == 0 or not self.size:
-            self.size = self.get_size()
-    
-    #TODO: Figure out page size
-    def get_size(self):
-        return 30
-        
     def __str__(self):
         return f"{self.title}"
+
