d90dfb0d41e59e321a90a7c53004aa519209c394
Chase-Warwick <warwick@ualberta.ca>
2021-10-16 17:06:59 -0400

    undo change to views api

--- socialdistribution/api/views.py
+++ socialdistribution/api/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from .models import User, Post
-from .serializers import PostSerializer, UserSerializer
+from .serializers import UserSerializer
 from urllib import parse
 
 @api_view(["GET"])
@@ -10,14 +10,6 @@ def author(request, author_id):
     author_id = parse.unquote(author_id.replace('_','%'))
     authorModel = get_object_or_404(User, pk=author_id)
     serializer = UserSerializer(authorModel)
-
-    return JsonResponse(serializer.data)
-
-@api_view(["GET"])
-def post(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    print(post)
-    serializer = PostSerializer(post)
 
     return JsonResponse(serializer.data)
 
