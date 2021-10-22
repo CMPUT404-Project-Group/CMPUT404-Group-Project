2905b0b3f40e08fdb214a5badaa75559aeafde98
xyL3 <36555005+xyL3@users.noreply.github.com>
2021-10-22 16:16:59 -0600

    Update forms.py

--- socialdistribution/app/forms.py
+++ socialdistribution/app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from api.models import Post, User
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
