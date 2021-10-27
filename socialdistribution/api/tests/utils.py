d66befd40e23833b52e7a3220143e856998af4f9
Dalton Ronan <dronan@ualberta.ca>
2021-10-26 23:09:47 -0600

    basic inbox test suite

--- socialdistribution/api/tests/utils.py
+++ socialdistribution/api/tests/utils.py
@@ -1,4 +1,4 @@
-from ..models import Post, User
+from ..models import Inbox, Post, User
 
 
 class TestUtils():
@@ -11,9 +11,7 @@ class TestUtils():
             type=type
         )
 
-    def get_test_post(
-            author=None, categories="test, categories, are, fun", image_content=None, text_content=None,
-            title="Test Title", visibility=Post.Visibility.PUBLIC, unlisted=False):
+    def get_test_post(author=None, categories="test, categories, are, fun", image_content=None, text_content=None, title="Test Title", visibility=Post.Visibility.PUBLIC, unlisted=False):
 
         if not author:
             author = TestUtils.get_test_user()
@@ -27,3 +25,10 @@ class TestUtils():
             visibility=visibility,
             unlisted=unlisted
         )
+
+    def setup_inbox(author_id, num_messages):
+        for i in range(num_messages):
+            content_object = TestUtils.get_test_post(
+                author=User.objects.get(id=author_id), text_content="Inbox Post %s" % i)
+            Inbox.objects.create(author_id=author_id,
+                                 content_object=content_object)
