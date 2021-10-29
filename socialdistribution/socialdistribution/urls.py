63cdbb9b0d5d5b7cc96c3168000c2150240f3fc2
Chase-Warwick <warwick@ualberta.ca>
2021-10-29 09:49:28 -0600

    Redirects from root to app root

--- socialdistribution/socialdistribution/urls.py
+++ socialdistribution/socialdistribution/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -25,4 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
     path('api/', include('api.urls')),
+    path('', lambda req: redirect('app:index')),    #https://stackoverflow.com/questions/7284952/django-redirect-to-root-from-a-view
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
