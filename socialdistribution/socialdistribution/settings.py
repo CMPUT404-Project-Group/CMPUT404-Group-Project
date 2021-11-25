748e918618d08ce25d456b389dff48dd0e93cfe4
Dalton Ronan <dronan@ualberta.ca>
2021-11-25 11:14:16 -0700

    fix

--- socialdistribution/socialdistribution/settings.py
+++ socialdistribution/socialdistribution/settings.py
@@ -143,6 +143,6 @@ AUTH_USER_MODEL = 'api.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 10
 }
 django_on_heroku.settings(locals(), test_runner=False) # bottom of the file
