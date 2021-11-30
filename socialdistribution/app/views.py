ec5166a50d89fe997f5a87e593f82d9bbe3cfebd
atonyma7 <atonyma7@gmail.com>
2021-11-30 13:57:45 -0700

    Adding author token

--- socialdistribution/app/views.py
+++ socialdistribution/app/views.py
@@ -185,13 +185,13 @@ def post(request, post_id):
 def foreign_post(request):
     data = request.POST.dict()
     post = Node_Interface.get_post(data['post'])
+    author_token = Node.objects.get(url=post.author.url]).auth_token
     context = {
         'post': post,
         'is_author': False,
         'user' : request.user,
         'token' : API_TOKEN,
-        #???
-        'author_token' : None
+        'author_token' : author_token
     }
     return render(request, 'posts/view_foreign_post.html', context)
 
