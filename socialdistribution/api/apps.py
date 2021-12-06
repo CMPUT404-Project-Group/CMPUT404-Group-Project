98c1ed46071c8153436b04fefba59327c3023d62
Chase-Warwick <warwick@ualberta.ca>
2021-12-06 11:32:58 -0700

    Move ready to app.py

--- socialdistribution/api/apps.py
+++ socialdistribution/api/apps.py
@@ -4,9 +4,3 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-
-    def ready(self):
-        from app import updater
-        updater.update_cached_posts()
-        updater.update_cached_authors()
-
